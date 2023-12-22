7972fb140cbd42fff097d8262e184eac2f3619ae
brayan <branyan.ubigo@gmail.com>
2023-12-22 18:24:52 -0300

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+const express= require('express')
+const app = express()
+app.disable('x-powered-by')
+
+const PORT = process.env.PORT ?? 1234
+
+
+app.get('/', (req,res) =>{
+    res.status(200).send('olaaaaasdfasdfsdfsafasdfaaa')
+})
+
+app.listen(PORT,()=>{
+ 
+})
