143075a526565572ea0fdec864ff125da8fc0a5b
brayan <branyan.ubigo@gmail.com>
2023-12-26 01:57:47 -0300

    conexion front end con backend

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,27 +1,64 @@
 import {pool} from '../db.js'
 
+
+export const getProductoById = async (req,res) =>{
+    
+    try {
+        const [rows] = await pool.query('SELECT * FROM producto WHERE id=?',[req.params.id]) 
+     if (rows.length <= 0 )return res.status(404).json({
+        message: 'Producto no encontrado'
+     })
+ res.send({rows})
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Something goes wrong'
+        })
+    }
+}
+
+
 export const getProducto = async (req,res) => {
-    const {nombre, precio, stock,idusuario } = req.body
-   const [rows] = await pool.query('SELECT * FROM producto') 
-    console.log(req.body)
-res.send({rows})
+
+try {
+   
+    const [rows] = await pool.query('SELECT * FROM producto') 
+     
+ res.send({rows})
+} catch (error) {
+    return res.status(500).json({
+        message: 'Something goes wrong'
+    })
+}
+ 
 }
 
 export const createProducto = async (req,res) => {
     const {nombre, precio, stock,idusuario } = req.body
-   const [rows] = await pool.query('INSERT INTO producto (nombre,precio, stock,idusuario) VALUES(?,?,?,?)',[nombre,precio,stock,idusuario]) 
-    console.log(req.body)
-res.send({
-    id: rows.insertId,
-    nombre,
-    precio,
-    stock,
-    idusuario
-})
+    try {
+        const [rows] = await pool.query('INSERT INTO producto (nombre,precio, stock,idusuario) VALUES(?,?,?,?)',[nombre,precio,stock,idusuario]) 
+         console.log(req.body)
+     res.send({
+         id: rows.insertId,
+         nombre,
+         precio,
+         stock,
+         idusuario
+     })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Something goes wrong'
+        })
+    }
+
+
 }
+
+
 export const updateProducto = async (req,res) => {
     const {id} = req.params
     const {nombre, precio, stock,idusuario } = req.body
+    try {
+  
     const [result] = await pool.query('UPDATE producto SET nombre = IFNULL(?,nombre), precio = IFNULL(?,precio), stock = IFNULL(?,stock) , idusuario = IFNULL(?,idusuario) WHERE id = ?', 
     [nombre,precio,stock,idusuario, id])
     console.log(result)
@@ -34,16 +71,31 @@ export const updateProducto = async (req,res) => {
 
     const [rows] = await pool.query('SELECT * FROM producto WHERE id = ?', [id])
     res.json(rows[0])
+   } catch (error) {
+    return res.status(500).json({
+        message: 'Something goes wrong'
+    })
+   }
+   
+  
 }
 
-export const deleteProducto =  async (req,res) => {
-    
-   const [result] = await pool.query('DELETE FROM producto WHERE id=? ', [req.params.id]) 
 
-   if (result.affectedRows <= 0  ){ 
-    return res.status(404).json({ message: 'Producto no encontrado'})
-   }
-    
-    res.sendStatus(204)
+
+export const deleteProducto =  async (req,res) => {
+    try {
+        const [result] = await pool.query('DELETE FROM producto WHERE id=? ', [req.params.id]) 
+
+        if (result.affectedRows <= 0  ){ 
+         return res.status(404).json({ message: 'Producto no encontrado'})
+        }
+         
+         res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Something goes wrong'
+        })
+    }
+ 
 
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {getProducto, createProducto, updateProducto, deleteProducto} from '../controllers/productos.controllers.js'
+import {getProducto, createProducto, updateProducto, deleteProducto, getProductoById} from '../controllers/productos.controllers.js'
 
 const router = Router()
 
 router.get('/getProductos',getProducto)
+
+router.get('/getProductos/:id',getProductoById)
 
 router.post('/postProductos',createProducto)
 
