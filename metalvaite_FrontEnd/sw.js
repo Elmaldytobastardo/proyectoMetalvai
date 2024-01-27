064dd2487820170caef635bca8e246f145b39d89
Diego <62708976+Elmaldytobastardo@users.noreply.github.com>
2024-01-27 08:33:49 -0300

    Delete metalvaite_FrontEnd directory

--- metalvaite_FrontEnd/sw.js
+++
@@ -1,7 +0,0 @@
-console.log('holaaaaaaaaa')
-self.addEventListener('push', e=>{
-    const data = e.data.json()
-    self.registration.showNotification(data.tittle,
-        {body: data.message}
-    )
-})
